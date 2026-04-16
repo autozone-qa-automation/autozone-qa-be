370cee7f8b4478f04a305d1f09d9176e82e8b411
Diego Terrazas <A01567189@tec.mx>
2026-04-15 19:35:58 -0700

    FeatureVOFix2

--- src/main/java/com/az_qa/backend/vo/FeatureVO.java
+++ src/main/java/com/az_qa/backend/vo/FeatureVO.java
@@ -3,87 +3,99 @@ package com.az_qa.backend.vo;
 import jakarta.validation.constraints.NotBlank;
 
 public class FeatureVO {
-    private Long id;
-    @NotBlank(message = "Feature name is required")
-    private String featureName;
-    @NotBlank(message = "Feature description is required")
-    private String featureDescription;
-    @NotBlank(message = "Feature service is required")
-    private String featureService;
+  private Long id;
 
-    public FeatureVO() {
-    }
+  @NotBlank(message = "Feature name is required")
+  private String featureName;
 
-    public FeatureVO(Long id, String featureName, String featureDescription, String featureService) {
-        this.id = id;
-        this.featureName = featureName;
-        this.featureDescription = featureDescription;
-        this.featureService = featureService;
-    }
+  @NotBlank(message = "Feature description is required")
+  private String featureDescription;
 
-    public Long getId() { 
-        return id;
-    }
+  @NotBlank(message = "Feature service is required")
+  private String featureService;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public FeatureVO() {}
 
-    public String getFeatureName() {
-        return featureName;
-    }
+  public FeatureVO(Long id, String featureName, String featureDescription, String featureService) {
+    this.id = id;
+    this.featureName = featureName;
+    this.featureDescription = featureDescription;
+    this.featureService = featureService;
+  }
 
-    public void setFeatureName(String featureName) {
-        this.featureName = featureName;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public String getFeatureDescription() {
-        return featureDescription;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public void setFeatureDescription(String featureDescription) {
-        this.featureDescription = featureDescription;
-    }
+  public String getFeatureName() {
+    return featureName;
+  }
 
-    public String getFeatureService() {
-        return featureService;
-    }
+  public void setFeatureName(String featureName) {
+    this.featureName = featureName;
+  }
 
-    public void setFeatureService(String featureService) {
-        this.featureService = featureService;
-    }
+  public String getFeatureDescription() {
+    return featureDescription;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+  public void setFeatureDescription(String featureDescription) {
+    this.featureDescription = featureDescription;
+  }
 
-        FeatureVO that = (FeatureVO) o;
+  public String getFeatureService() {
+    return featureService;
+  }
 
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (featureName != null ? !featureName.equals(that.featureName) : that.featureName != null) return false;
-        if (featureDescription != null ? !featureDescription.equals(that.featureDescription) : that.featureDescription != null)
-            return false;
-        return featureService != null ? featureService.equals(that.featureService) : that.featureService == null;
-    }
+  public void setFeatureService(String featureService) {
+    this.featureService = featureService;
+  }
 
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (featureName != null ? featureName.hashCode() : 0);
-        result = 31 * result + (featureDescription != null ? featureDescription.hashCode() : 0);
-        result = 31 * result + (featureService != null ? featureService.hashCode() : 0);
-        return result;
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
 
-    @Override
-    public String toString() {
-        return "FeaturesVO{" +
-                "id=" + id +
-                ", featureName='" + featureName + '\'' +
-                ", featureDescription='" + featureDescription + '\'' +
-                ", featureService='" + featureService + '\'' +
-                '}';
-    }
+    FeatureVO that = (FeatureVO) o;
 
+    if (id != null ? !id.equals(that.id) : that.id != null) return false;
+    if (featureName != null ? !featureName.equals(that.featureName) : that.featureName != null)
+      return false;
+    if (featureDescription != null
+        ? !featureDescription.equals(that.featureDescription)
+        : that.featureDescription != null) return false;
+    return featureService != null
+        ? featureService.equals(that.featureService)
+        : that.featureService == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id != null ? id.hashCode() : 0;
+    result = 31 * result + (featureName != null ? featureName.hashCode() : 0);
+    result = 31 * result + (featureDescription != null ? featureDescription.hashCode() : 0);
+    result = 31 * result + (featureService != null ? featureService.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "FeatureVO{"
+        + "id="
+        + id
+        + ", featureName='"
+        + featureName
+        + '\''
+        + ", featureDescription='"
+        + featureDescription
+        + '\''
+        + ", featureService='"
+        + featureService
+        + '\''
+        + '}';
+  }
 }
