ea661d9932477f1c4fc9878850289d30ceba1f08
santi-2000 <A01569193@tec.mx>
2026-04-17 18:31:07 -0600

    -m leer services controller, entity, service, vo

---
+++ src/main/java/com/az_qa/backend/entity/ServicesEntity.java
@@ -0,0 +1,92 @@
+/*
+Tecnológico de Monterrey — Campus Chihuahua
+Desarrollo e Implantación de Sistemas de Software
+TC3005B GPO500 - 2026
+Autozone QA Automation
+*/
+package com.az_qa.backend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "services")
+public class ServicesEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "description", nullable = true, length = 255)
+    private String description;
+
+    public ServicesEntity() {
+    }
+
+    public ServicesEntity(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "ServicesEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        ServicesEntity that = (ServicesEntity) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null)
+            return false;
+        if (name != null ? !name.equals(that.name) : that.name != null)
+            return false;
+        return description != null ? description.equals(that.description) : that.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+}
