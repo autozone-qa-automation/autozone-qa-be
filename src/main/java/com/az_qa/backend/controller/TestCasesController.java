0ad8f8ccf0abea0337657e173b1b7f26982cce00
JEDLP <a01569171@tec.mx>
2026-04-21 19:16:37 -0600

    prueba

--- src/main/java/com/az_qa/backend/controller/TestCasesController.java
+++ src/main/java/com/az_qa/backend/controller/TestCasesController.java
@@ -8,8 +8,11 @@ Autozone QA Automation
 
 package com.az_qa.backend.controller;
 
+import com.az_qa.backend.service.TestCasesService;
+import com.az_qa.backend.vo.TestCaseVO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -20,12 +23,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.az_qa.backend.service.TestCasesService;
-import com.az_qa.backend.vo.TestCaseVO;
-
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Positive;
 
 @RestController
 @RequestMapping("/api/v1/test-cases")
