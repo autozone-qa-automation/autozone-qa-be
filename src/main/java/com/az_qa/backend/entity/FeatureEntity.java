1d4958c79ad1c7cd7288374fa46f7b67a65e6040
Diego Terrazas <A01567189@tec.mx>
2026-04-23 18:24:16 -0700

    FixfeatureEntity

--- src/main/java/com/az_qa/backend/entity/FeatureEntity.java
+++ src/main/java/com/az_qa/backend/entity/FeatureEntity.java
@@ -10,12 +10,14 @@ package com.az_qa.backend.entity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
@@ -39,6 +41,10 @@ public class FeatureEntity {
 
   @Column(name = "idServices")
   private long idServices;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "idServices")
+  private ServicesEntity services;
 
   @OneToMany(mappedBy = "feature", cascade = CascadeType.ALL)
   private List<TestCasesEntity> testCases = new ArrayList<>();
