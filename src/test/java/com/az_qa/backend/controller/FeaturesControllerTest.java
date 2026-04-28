ed79284594cbdd0c34dd14fca4397b9705b96c0b
Diego Terrazas <A01567189@tec.mx>
2026-04-28 16:17:44 -0700

    LintheaderFix

--- src/test/java/com/az_qa/backend/controller/FeaturesControllerTest.java
+++ src/test/java/com/az_qa/backend/controller/FeaturesControllerTest.java
@@ -1,3 +1,10 @@
+/*
+Tecnológico de Monterrey — Campus Chihuahua
+Desarrollo e Implantación de Sistemas de Software
+TC3005B GPO500 - 2026
+Autozone QA Automation
+*/
+
 package com.az_qa.backend.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
